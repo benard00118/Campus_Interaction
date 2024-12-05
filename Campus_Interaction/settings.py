184331b9aae5311e83a19c091f9cc59ad17a063e
Benard Karanja <benardopeter4@gmail.com>
2024-12-05 11:05:25 +0300

    Production version

--- Campus_Interaction/settings.py
+++ Campus_Interaction/settings.py
@@ -78,6 +78,9 @@ CHANNEL_LAYERS = {
         'CONFIG': {
             "hosts": [("localhost", 6379)],
         },
+        'OPTIONS': {
+            'timeout': 10,  
+        }
     },
 }
 
@@ -149,9 +152,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "camphub.ke@gmail.com"
-EMAIL_HOST_PASSWORD = "daqz qzkm ximp xpiy"
-DEFAULT_FROM_EMAIL = "camphub.ke@gmail.com"
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
+
+
 
 
 # Authentication Backend
