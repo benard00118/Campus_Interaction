6c1670dc0791d141b93bd7551a9f15457f721c74
audo-wrld <audo-wrld@kali>
2024-12-06 15:57:12 +0300

    Recent searches model for saving recent searched places

--- maps/urls.py
+++ maps/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = "maps"
 urlpatterns = [
-    path('', views.maps, name='maps') 
+    path('', views.maps, name='maps'),
+    path('save-search/', views.save_search, name='save_search'),
+    path('recent-searches/', views.recent_searches, name='recent_searches'),
 ]
